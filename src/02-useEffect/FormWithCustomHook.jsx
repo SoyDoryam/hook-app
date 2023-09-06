fd8dc7336847320c5d80e9c237cc232a6f7aced5
Doryam <rizo0655@gmail.com>
2023-09-05 21:38:35 -0600

    useFetch - CustomHook

--- src/02-useEffect/FormWithCustomHook.jsx
+++ src/02-useEffect/FormWithCustomHook.jsx
@@ -2,13 +2,15 @@ import { useEffect } from "react"
 import { useForm } from "../hooks/useForm";
 
 export const FormWithCustomHook = () => {
-    // 1. Utilización del custom hook useForm para gestionar el formulario
-    const { formState, onInputChange, onResetForm, username, email, password } = useForm({
+
+    const {formState, onInputChange, onResetForm, username, email, password} = useForm({
         username: '',
         email: '',
         password: '',
     });    
 
+    // const {username, email, password} = formState;
+    
     return (
         <>
             <h1>Simple Form</h1>
@@ -20,7 +22,6 @@ export const FormWithCustomHook = () => {
                 placeholder="Username"
                 name="username"
                 value={ username }
-                // 2. Manejo de cambios en el input con la función onInputChange
                 onChange={ onInputChange } 
             />
 
@@ -43,7 +44,6 @@ export const FormWithCustomHook = () => {
             />
 
             <button
-                // 3. Manejo del clic en el botón para restablecer el formulario
                 onClick={ onResetForm } 
                 className="btn btn-primary mt-2"
             >Borrar</button>
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,30 +1,25 @@
 import { useState } from "react";
 
 export const useForm = (initialForm = {}) => {
-    // 1. Utilización de useState para gestionar el estado del formulario
+
     const [formState, setFormState] = useState(initialForm);
 
-    // 2. Definición de la función onInputChange para manejar cambios en los inputs
     const onInputChange = ({target}) => {
         const {name, value} = target;
-        // 3. Actualización del estado del formulario al cambiar un input
         setFormState({
-            ...formState, // 4. Preservación del estado actual
-            [name]: value // 5. Actualización del campo específico
+            ...formState,
+            [ name ]: value
         });
     };
 
-    // 6. Definición de la función onResetForm para restablecer el formulario
     const onResetForm = () => {
-        // 7. Restablecimiento del formulario al estado inicial
-        setFormState(initialForm);
+        setFormState( initialForm );
     };
 
-    // 8. Retorno de los elementos necesarios para gestionar el formulario
     return {
-        ...formState, // 9. Retorno de los campos del formulario
-        formState, // 10. Retorno del estado del formulario (redundante)
-        onInputChange, // 11. Retorno de la función para manejar cambios en los inputs
-        onResetForm, // 12. Retorno de la función para restablecer el formulario
+        ...formState,
+        formState,
+        onInputChange,
+        onResetForm,
     }
 }
