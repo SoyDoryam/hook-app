93f8bb52035c76fd173e613243da2b6c7d0b15a0
Doryam <rizo0655@gmail.com>
2023-09-05 20:16:36 -0600

    Implementar funcionalidad del reset

--- src/02-useEffect/FormWithCustomHook.jsx
+++ src/02-useEffect/FormWithCustomHook.jsx
@@ -1,47 +1,52 @@
-import { useEffect } from "react";
+import { useEffect } from "react"
 import { useForm } from "../hooks/useForm";
 
 export const FormWithCustomHook = () => {
-    // Utiliza el custom hook useForm para gestionar el estado del formulario
-    const { formState, onInputChange, username, email, password } = useForm({
-        username: "",
-        email: "",
-        password: "",
-    });
-
-    // Desestructura las propiedades necesarias del objeto retornado por useForm
+    // 1. Utilización del custom hook useForm para gestionar el formulario
+    const { formState, onInputChange, onResetForm, username, email, password } = useForm({
+        username: '',
+        email: '',
+        password: '',
+    });    
 
     return (
         <>
             <h1>Simple Form</h1>
             <hr />
 
-            <input
+            <input 
                 type="text"
                 className="form-control"
                 placeholder="Username"
                 name="username"
-                value={username}
-                onChange={onInputChange}
+                value={ username }
+                // 2. Manejo de cambios en el input con la función onInputChange
+                onChange={ onInputChange } 
             />
 
-            <input
+            <input 
                 type="email"
                 className="form-control mt-2"
                 placeholder="rizo@google.com"
-                name="email"
-                value={email}
-                onChange={onInputChange}
+                name="email" 
+                value={ email }
+                onChange={ onInputChange }
             />
 
-            <input
+            <input 
                 type="password"
                 className="form-control mt-2"
                 placeholder="Contraseña"
-                name="password"
-                value={password}
-                onChange={onInputChange}
+                name="password" 
+                value={ password }
+                onChange={ onInputChange }
             />
+
+            <button
+                // 3. Manejo del clic en el botón para restablecer el formulario
+                onClick={ onResetForm } 
+                className="btn btn-primary mt-2"
+            >Borrar</button>
         </>
-    );
-};
+    )
+}
