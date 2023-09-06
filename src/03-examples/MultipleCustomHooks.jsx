593e484a866126015b533f145eb880cdfd87b1c4
Doryam <rizo0655@gmail.com>
2023-09-05 21:56:13 -0600

    useFetch - CustomHook

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,11 +1,16 @@
+// Importación del Custom Hook useFetch desde la ubicación especificada
 import { useFetch } from "../hooks/useFetch"
 
+// Componente funcional MultipleCustomHooks
 export const MultipleCustomHooks = () => {
 
+    // Utilización del Custom Hook useFetch para obtener datos
     const { data, isLoading, hasError } = useFetch('https://api.breakingbadquotes.xyz/v1/quotes/1');
 
+    // Impresión en la consola de los datos y estados obtenidos
     console.log({ data, isLoading, hasError })
 
+    // Componente retorna una estructura JSX
     return (
         <>
             <h1>BreakingBad Quotes</h1>
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,38 +1,45 @@
+// Importación de los hooks necesarios de React
 import { useEffect, useState } from "react";
 
-export const useFetch = ( url ) => {
-
+// Declaración y definición del Custom Hook useFetch
+export const useFetch = (url) => {
+    
+    // Estado inicial con valores predeterminados
     const [state, setState] = useState({
-        data: null,
-        isLoading: true,
-        hasError: null,
+        data: null,         // Almacena los datos obtenidos de la petición
+        isLoading: true,    // Indica si la petición está en curso
+        hasError: null,     // Almacena errores en caso de que ocurran
     });
 
+    // Función para realizar la petición HTTP y actualizar el estado
     const getFetch = async () => {
-
+        // Cambio del estado isLoading a true (indicando inicio de la petición)
         setState({
             ...state,
             isLoading: true,
         });
 
-        const resp = await fetch( url );
-        const data = await resp.json();
+        // Realización de la petición HTTP utilizando fetch
+        const resp = await fetch(url);   // Realiza la petición
+        const data = await resp.json();  // Procesa la respuesta como JSON
 
+        // Actualización del estado con los datos obtenidos
         setState({
-            data,
-            isLoading: false,
-            hasError: null,
-        });    
+            data,                       // Almacena los datos de la respuesta
+            isLoading: false,           // Indica que la petición ha finalizado
+            hasError: null,             // Restablece cualquier error previo
+        });
     } 
 
+    // Efecto que se dispara cuando la URL cambia
     useEffect(() => {
-        getFetch();
-    }, [url])
-    
+        getFetch();  // Llama a la función getFetch para realizar la petición
+    }, [url])  // Dependencia que activa el efecto cuando la URL cambia
 
+    // Devuelve un objeto con los estados para su uso en componentes
     return {
-        data: state.data,
-        isLoading: state.isLoading,
-        hasError: state.hasError
+        data: state.data,          // Datos obtenidos de la petición
+        isLoading: state.isLoading,  // Indicador de carga de la petición
+        hasError: state.hasError,    // Información sobre errores, si los hay
     };
 }
