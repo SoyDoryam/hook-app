3413a26aa7240f3bf94f994cb4247e684c0b83df
Doryam <rizo0655@gmail.com>
2023-09-05 23:10:10 -0600

    useFetch + useCounter

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,10 +1,11 @@
 import { useFetch } from "../hooks/useFetch"
 
 export const MultipleCustomHooks = () => {
-
+    // 1. Utilización del hook personalizado useFetch para realizar una solicitud a la API
     const { data, isLoading, hasError } = useFetch('https://api.breakingbadquotes.xyz/v1/quotes/1');
 
-    const {author, quote} = !!data && data[0];
+    // 2. Desestructuración de la respuesta para obtener el autor y la cita
+    const { author, quote } = !!data && data[0];
 
     return (
         <>
@@ -12,18 +13,18 @@ export const MultipleCustomHooks = () => {
             <hr />
 
             {
+                // 3. Renderizado condicional basado en el estado de isLoading
                 isLoading ? (
                     <div className="alert alert-info text-center">
                         Loading...
                     </div>
-                ):(
+                ) : (
                     <blockquote className="blockquote text-end">
                         <p className="mb-1">{ quote }</p>
                         <footer className="blockquote-footer">{ author }</footer>
                     </blockquote>
                 )
             }
-
 
             <button className="btn btn-primary">
                 Next quote
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,38 +1,42 @@
 import { useEffect, useState } from "react";
 
-export const useFetch = ( url ) => {
-
+export const useFetch = (url) => {
+    // 1. Inicialización del estado usando useState
     const [state, setState] = useState({
         data: null,
         isLoading: true,
         hasError: null,
     });
 
+    // 2. Definición de la función asincrónica getFetch
     const getFetch = async () => {
-
+        // 3. Actualización del estado para indicar que la carga está en curso
         setState({
             ...state,
             isLoading: true,
         });
 
-        const resp = await fetch( url );
+        // 4. Realización de la solicitud a la URL utilizando fetch
+        const resp = await fetch(url);
         const data = await resp.json();
 
+        // 5. Actualización del estado con los datos recibidos
         setState({
             data,
-            isLoading: false,
-            hasError: null,
-        });    
-    } 
+            isLoading: false, // 6. Indicación de que la carga ha finalizado
+            hasError: null, // 6. Reinicio del posible error a null
+        });
+    };
 
+    // 7. Utilización de useEffect para realizar la solicitud cuando la URL cambie
     useEffect(() => {
         getFetch();
-    }, [url])
-    
+    }, [url]);
 
+    // 8. Retorno de un objeto con el estado actualizado
     return {
         data: state.data,
         isLoading: state.isLoading,
         hasError: state.hasError,
     };
-}
+};
