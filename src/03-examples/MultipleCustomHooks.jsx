9ee1cf08ac71711b763397727f1bc5dbe5257bfd
Doryam <rizo0655@gmail.com>
2023-09-13 13:39:59 -0600

    Optimizaciones al código anterior

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,15 +1,15 @@
-import { useCounter } from "../hooks/useCounter";
-import { useFetch } from "../hooks/useFetch"
+import { useCounter, useFetch } from "../hooks";
+import { LoadingQuote, Quote } from "./";
 
 export const MultipleCustomHooks = () => {
 
-    // Usamos el gancho useCounter para inicializar un contador con un valor inicial de 1
+    // Utiliza el gancho useCounter para administrar el contador
     const { counter, increment } = useCounter(1);
 
-    // Usamos el gancho useFetch para realizar una solicitud a una API con la URL que incluye el valor actual de counter
+    // Utiliza el gancho useFetch para realizar una solicitud a la API
     const { data, isLoading, hasError } = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes/${ counter }`);
 
-    // Desestructuramos la respuesta de la solicitud para obtener el autor y la cita
+    // Extrae la información del autor y la cita de los datos obtenidos de la API
     const { author, quote } = !!data && data[0];
 
     return (
@@ -17,27 +17,18 @@ export const MultipleCustomHooks = () => {
             <h1>BreakingBad Quotes</h1>
             <hr />
 
-            {/* Usamos una expresión ternaria para mostrar un mensaje de carga o la cita real según isLoading */}
-            {isLoading ? (
-                <div className="alert alert-info text-center">
-                    Loading...
-                </div>
-            ) : (
-                // Mostramos la cita y el autor dentro de un bloque de citas
-                <blockquote className="blockquote text-end">
-                    <p className="mb-1">{ quote }</p>
-                    <footer className="blockquote-footer">{ author }</footer>
-                </blockquote>
-            )}
+            {/* Renderiza el componente LoadingQuote si isLoading es verdadero, de lo contrario, muestra el componente Quote */}
+            { isLoading 
+                ? <LoadingQuote />
+                : <Quote author={ author } quote={ quote } />
+            }
 
-            {/* El botón "Next quote" se habilita o deshabilita según isLoading y llama a la función increment al hacer clic */}
+            {/* Botón para obtener la siguiente cita */}
             <button 
                 className="btn btn-primary"
-                disabled={ isLoading } // Deshabilita el botón si isLoading es true
-                onClick={() => increment()} // Llama a la función increment al hacer clic
-            >
-                Next quote {counter} {/* Muestra el valor actual de counter junto al texto del botón */}
-            </button>
+                disabled={ isLoading } // Deshabilita el botón durante la carga
+                onClick={() => increment()} // Incrementa el contador al hacer clic
+            >Next quote { counter }</button>
         </>
     )
 }
