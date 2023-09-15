0503b65fe0438142f0af641aca40e3adf578e2e9
Doryam <rizo0655@gmail.com>
2023-09-15 16:07:15 -0600

    Creando el cascarón de la lista de TODOs

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,6 +1,7 @@
-import { useReducer } from "react"
+import { useReducer } from "react";
 import { todoReducer } from "./todoReducer";
 
+// Estado inicial de la lista de tareas.
 const initialState = [
     {
         id: new Date().getTime(),
@@ -12,23 +13,49 @@ const initialState = [
         description: 'Recolectar la piedra del alma',
         done: false
     },
-]
+];
 
 export const TodoApp = () => {
 
-    const [state, dispatch] = useReducer( todoReducer, initialState);
+    // Usamos el hook useReducer para gestionar el estado de la lista de tareas.
+    // Pasamos el reducer (todoReducer) y el estado inicial (initialState) como argumentos.
+    const [state, dispatch] = useReducer(todoReducer, initialState);
 
 
     return (
         <>
-            <h1>TodoApp</h1>
+            <h1>TodoApp 10, <small>pendientes: 2</small></h1>
             <hr />
 
-            <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-            </ul>
+        <div className="row">
+            <div className="col-7">
+                <ul className="list-group">
+                    <li className="list-group-item d-flex justify-content-between">
+                        <span className="align-self-center">Item 1</span>
+                        <button className="btn btn-danger">Borrar</button>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="col-5">
+                <h4>Agregar TODO</h4>
+                <hr />
+                <form >
+                    <input 
+                        type="text" 
+                        placeholder="¿Que hay que hacer?"
+                        className="form-control"
+                    />
+                </form>
+
+                <button
+                    type="submit"
+                    className="btn btn-outline-primary mt-1"
+                >
+                    Agregar
+                </button>
+            </div>
+        </div>
         </>
-    )
+    );
 }
--- src/08-useReducer/todoReducer.js
+++ src/08-useReducer/todoReducer.js
@@ -1,10 +1,14 @@
-export const todoReducer = ( initialState = [], action) => {
+// Definición del reducer. Recibe el estado inicial y una acción como argumentos.
+export const todoReducer = (initialState = [], action) => {
     
+    // Utiliza un switch para manejar diferentes tipos de acciones.
     switch (action.type) {
         case 'ABC':
-            throw new Error('Action.type = ABC no esta implementada');
+            // Cuando se recibe una acción de tipo 'ABC', lanzamos un error indicando que esta acción no está implementada.
+            throw new Error('Action.type = ABC no está implementada');
     
         default:
+            // En caso de cualquier otra acción, simplemente se devuelve el estado inicial sin cambios.
             return initialState;
     }
 }
