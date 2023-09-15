6068960608bc929192bce474c1f6589398dffe20
Doryam <rizo0655@gmail.com>
2023-09-15 10:42:50 -0600

    useCallback (comentarios explicativos)

--- src/06-memos/CallbackHook.jsx
+++ src/06-memos/CallbackHook.jsx
@@ -1,26 +1,27 @@
-import { useCallback, useState } from "react"
+import { useCallback, useState } from "react";
 import { ShowIncrement } from "./ShowIncrement";
 
 export const CallbackHook = () => {
+    // Inicializamos el estado del contador en 10.
+    const [counter, setCounter] = useState(10);
 
-    const [counter, setCounter] = useState( 10 );
-
-    const incrementFather =  useCallback(
-      () => {
-        setCounter( (value) => value + 1 );
-      },
-      [],
+    // Utilizamos useCallback para memorizar la función incrementFather.
+    const incrementFather = useCallback(
+        () => {
+            // Incrementamos el valor del contador en 1 utilizando la función de actualización
+            // para asegurarnos de que estamos obteniendo el valor más reciente.
+            setCounter((value) => value + 1);
+        },
+        [] // La dependencia está vacía, lo que significa que esta función se memoriza una vez y no cambia.
     );
 
-    // const incrementFather = () => {
-    //     setCounter( counter + 1 );
-    // }
+    // El componente muestra el valor del contador y el componente ShowIncrement que recibe la función incrementFather como prop.
 
     return (
         <>
-            <h1>useCallback Hook: { counter }</h1>
+            <h1>useCallback Hook: {counter}</h1>
             <hr />
-            <ShowIncrement increment={ incrementFather }/>
+            <ShowIncrement increment={incrementFather} />
         </>
-    )
-}
+    );
+};
--- src/06-memos/ShowIncrement.jsx
+++ src/06-memos/ShowIncrement.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 
-export const ShowIncrement = React.memo( ({ increment }) => {
+// Utilizamos React.memo para memorizar este componente y evitar
+// que se vuelva a renderizar innecesariamente cuando las props no cambian.
+const ShowIncrement = React.memo(({ increment }) => {
+  // Este mensaje se imprimirá cada vez que el componente se vuelva a renderizar.
+  console.log('Me volví a generar :(');
 
-  console.log('Me volvi a generar :(')
   return (
     <button
-        className="btn btn-primary"
-        onClick={() => {
-            increment();
-        }}
+      className="btn btn-primary"
+      onClick={() => {
+        // Cuando se hace clic en el botón, se ejecuta la función increment
+        // que se pasa como prop desde el componente padre.
+        increment();
+      }}
     >
-        Increment
+      Increment
     </button>
-  )
-})
+  );
+});
