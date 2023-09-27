0e862551cfc259dfafe301beebdc82f73b574587
Doryam <rizo0655@gmail.com>
2023-09-27 12:14:57 -0600

    Link

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -2,27 +2,22 @@ import { Navigate, Route, Routes } from "react-router-dom"
 import { HomePage } from "./HomePage"
 import { AboutPage } from "./AboutPage"
 import { LoginPage } from "./LoginPage"
+import { NavBar } from "./NavBar"
 
 export const MainApp = () => {
   return (
     <>
         <h1>MainApp</h1>
+        <NavBar />
         <hr />
 
-        {/* Definición de las rutas */}
         <Routes>
-          {/* Ruta para la página de inicio */}
           <Route path="/" element={<HomePage />} />
-          
-          {/* Ruta para la página "Acerca de" */}
           <Route path="about" element={<AboutPage />} />
-          
-          {/* Ruta para la página de inicio de sesión */}
           <Route path="login" element={<LoginPage />} />
 
-          {/* Ruta comodín para manejar rutas no reconocidas */}
-          {/* En este caso, redirige a la página "Acerca de" */}
-          <Route path="/*" element={<Navigate to="/about" />} />
+          {/* <Route path="/*" element={ <LoginPage />}/> */}
+          <Route path="/*" element={ <Navigate to="/about" />}/>
         </Routes>
     </>
   )
