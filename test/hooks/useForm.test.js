04f714b176f8556c6dbe15e2953bb63762e2d6f1
Doryam <rizo0655@gmail.com>
2023-10-05 21:04:38 -0600

    Pruebas sobre useForm - CustomHook agregar comentarios

--- test/hooks/useForm.test.js
+++ test/hooks/useForm.test.js
@@ -2,14 +2,19 @@ import { act, renderHook } from "@testing-library/react";
 import { useForm } from "../../src/hooks/useForm";
 
 describe('Pruebas en useForm', () => { 
+    // Definir un formulario de ejempo para las pruebas
     const initialForm = {
         name: 'Dorian Rizo',
         email: 'dorian@rizo.com'
 
-    } 
+    };
 
+    // Pruebas para verificar si el hook retorna los valores por defectio 
     test('debe de regresar los valores por defecto', () => { 
+        // Renderiza el hook useForm con el formulario inicial
         const { result } = renderHook( () => useForm( initialForm ) );
+
+        // Verifica que el resultado del hook coincida con los valores esperados
         expect(result.current).toEqual({
             name: initialForm.name,
             email: initialForm.email,
@@ -19,27 +24,35 @@ describe('Pruebas en useForm', () => {
         });
     });
 
+    // Prueba para verificar si el nomnbre del formulario cambia correctamente
     test('debe de cambiar el nombre del formulario ', () => { 
         const newValue = 'Dorian Rizo';
          const { result  } = renderHook(() => useForm( initialForm ) );
          const { onInputChange } = result.current;
+
+         // Simula el cambio del nombre del formulario 
          act( () => {
             onInputChange({ target: { name: 'name',value: newValue } })
          });
 
+         // Verifica que el nombre haya cambiado correctamente
         expect(result.current.name).toBe( newValue );;
         expect(result.current.formState.name ).toBe( newValue );;
      })
     
+     // Prueba para verificar si se realiza el reseteo del formualrio
      test('debe de realizar el reset del formulario', () => { 
         const newValue = 'Dorian Rizo';
          const { result  } = renderHook(() => useForm( initialForm ) );
          const { onInputChange, onResetForm } = result.current;
-         act( () => {
+
+        // Simula un cambio en el nombre del formulario y luego lo resetea
+        act( () => {
             onInputChange({ target: { name: 'name',value: newValue } });
             onResetForm();
-         });
+        });
 
+        // Verifica que el nombre haya vuelto al valor inicial despues del reset
         expect(result.current.name).toBe( initialForm.name );;
         expect(result.current.formState.name ).toBe( initialForm.name );;
      })
