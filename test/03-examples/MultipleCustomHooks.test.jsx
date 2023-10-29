a3237d6b20f1f0971d14c93d68d2abbf1009fc58
Doryam <rizo0655@gmail.com>
2023-10-29 14:54:34 -0600

    Evaluar respuesta del useFetch

--- test/03-examples/MultipleCustomHooks.test.jsx
+++ test/03-examples/MultipleCustomHooks.test.jsx
@@ -1,12 +1,36 @@
 // Importación de funciones y componentes necesarios para las pruebas
-import { render, screen } from '@testing-library/react'; // Importa funciones de React Testing Library
+import { fireEvent, render, renderHook, screen } from '@testing-library/react'; // Importa funciones de React Testing Library
 import { MultipleCustomHooks } from '../../src/03-examples'; // Importa el componente que se va a probar
+import { useFetch } from '../../src/hooks/useFetch';
+import { useCounter } from '../../src/hooks/useCounter';
+
+
+jest.mock('../../src/hooks/useFetch');
+jest.mock('../../src/hooks/useCounter');
 
 // Bloque de pruebas: Agrupa varias pruebas relacionadas bajo una descripción
 describe('Pruebas en <MultipleCustomHooks />', () => {
 
+    const mockIncrement = jest.fn();
+
+    useCounter.mockReturnValue({
+      counter: 1,
+      increment: mockIncrement,
+    });
+
+    beforeEach( () => {
+        jest.clearAllMocks()
+    } );
+
     // Prueba individual: Comprueba el comportamiento inicial del componente
     test('debe de mostrar el componente por defecto', () => { 
+
+        useFetch.mockReturnValue({ 
+            data: null,
+            isLoading: true,
+            hasError: null,
+         });
+
         // Renderiza el componente MultipleCustomHooks
         render(<MultipleCustomHooks />);
 
@@ -21,5 +45,36 @@ describe('Pruebas en <MultipleCustomHooks />', () => {
         //    Luego, verifica que el botón esté deshabilitado (propiedad 'disabled' establecida en true)
         const nextButton = screen.getByRole('button', { name: 'Next quote' });
         expect(nextButton.disabled).toBe(true);
+    });
+
+    test('debe de mostrar un Quote', () => {
+        useFetch.mockReturnValue({
+            data: [{ author: 'Fernando', quote: 'Hola Mundo' }],
+            isLoading: false,
+            hasError: null,
+        });
+
+        render( <MultipleCustomHooks /> );
+        expect( screen.getByText('Hola Mundo') ).toBeTruthy();
+        expect( screen.getByText('Fernando') ).toBeTruthy(); 
+
+        const nextButton = screen.getByRole('button', { name: 'Next quote' });
+        expect(nextButton.disabled).toBeFalsy();
+    })
+
+    test('debe de llamar la fucion de incrementar', () => {
+
+        useFetch.mockReturnValue({
+            data: [{ author: 'Dorian', quote: 'Hola Mundo'}],
+            isLoading: false,
+            hasError: null,
+        });
+
+        render( <MultipleCustomHooks /> );
+        
+        const nextButton = screen. getByRole('button', {name: 'Next quote'});
+        fireEvent.click( nextButton );
+
+        expect( mockIncrement ).toHaveBeenCalled();
     });
 });
