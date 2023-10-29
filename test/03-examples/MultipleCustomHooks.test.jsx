26d53129c06a00801e90ac38e6f135a048723ec9
Doryam <rizo0655@gmail.com>
2023-10-29 14:03:33 -0600

    Pruebas con múltiples hooks simultáneos comentarios

--- test/03-examples/MultipleCustomHooks.test.jsx
+++ test/03-examples/MultipleCustomHooks.test.jsx
@@ -1,14 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import { MultipleCustomHooks } from '../../src/03-examples';
+// Importación de funciones y componentes necesarios para las pruebas
+import { render, screen } from '@testing-library/react'; // Importa funciones de React Testing Library
+import { MultipleCustomHooks } from '../../src/03-examples'; // Importa el componente que se va a probar
 
-describe('Pruebas en <MultipleCustomHooks />', () => { 
-    test('debe de mostra el componente por defecto', () => { 
-         render(<MultipleCustomHooks />);
-         expect( screen.getByText('Loading...') );
-         expect( screen.getByText('BreakingBad Quotes') );
-         
-        const nextButton = screen.getByRole('button', { name: 'Next quote'});
+// Bloque de pruebas: Agrupa varias pruebas relacionadas bajo una descripción
+describe('Pruebas en <MultipleCustomHooks />', () => {
+
+    // Prueba individual: Comprueba el comportamiento inicial del componente
+    test('debe de mostrar el componente por defecto', () => { 
+        // Renderiza el componente MultipleCustomHooks
+        render(<MultipleCustomHooks />);
+
+        // Expectativas (assertions):
+        // 1. Verifica que el componente contenga el texto 'Loading...'
+        expect(screen.getByText('Loading...'));
+
+        // 2. Verifica que el componente contenga el texto 'BreakingBad Quotes'
+        expect(screen.getByText('BreakingBad Quotes'));
+
+        // 3. Busca un botón con el atributo 'role' igual a 'button' y nombre 'Next quote'
+        //    Luego, verifica que el botón esté deshabilitado (propiedad 'disabled' establecida en true)
+        const nextButton = screen.getByRole('button', { name: 'Next quote' });
         expect(nextButton.disabled).toBe(true);
-
     });
- });
+});
